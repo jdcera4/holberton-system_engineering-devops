3dc819e951514ff0bd8d97b0702135d6c24f2b4e
jdcera4 <jdcera4@gmail.com>
2021-08-17 22:04:30 -0400

    configuration nginx whitc puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,18 @@
+package{ 'nginx':
+ensure => installed,
+}
+
+file_line{ 'Add redirection':
+ensure => 'present',
+path   => '/etc/nginx/sites-available/default',
+after  => 'listen 80 default_server',
+}
+
+file{'/var/www/html/index.html':
+content => 'Holberton School'
+}
+
+service{'nginx':
+ensure  => running,
+require => Package['nginx']
+}
