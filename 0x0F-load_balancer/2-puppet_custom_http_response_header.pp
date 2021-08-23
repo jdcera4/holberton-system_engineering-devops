8d003e895de402c01d85d6fb2b3868272b037e5e
jdcera4 <jdcera4@gmail.com>
2021-08-23 15:37:29 -0400

    configuration headder with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,31 @@
+# install ngnx whit puppet and configuration
+exec {'apt-get-update':
+  command => '/usr/bin/apt-get update'
+}
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'Add redirection, 301':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file_line {'add header'.
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $HOSTNAME;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
