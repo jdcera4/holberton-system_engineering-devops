9624c5c2db35bfbe6f3ff24cffeca6b2b9b1c1f5
jdcera4 <jdcera4@gmail.com>
2021-08-23 15:48:09 -0400

    file modified

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,13 +1,11 @@
-# install ngnx whit puppet and configuration
-exec {'apt-get-update':
+# puppet manifest creating a custom HTTP header response
+exec { 'apt-get-update':
   command => '/usr/bin/apt-get update',
 }
-
 package { 'nginx':
   ensure  => installed,
-  require => Exec['apt-get update'],
+  require => Exec['apt-get-update'],
 }
-
 file_line { 'a':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
@@ -15,20 +13,17 @@ file_line { 'a':
   line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
   require => Package['nginx'],
 }
-
-file_line {'b':
+file_line { 'b':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $HOSTNAME;',
+  line    => 'add_header X-Served-By $hostname;',
   require => Package['nginx'],
 }
-
 file { '/var/www/html/index.html':
   content => 'Holberton School',
   require => Package['nginx'],
 }
-
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
