169d6be1c8af9eabf34061c335fb46eb1a772ba1
jdcera4 <jdcera4@gmail.com>
2021-08-23 15:44:34 -0400

    . por :

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -16,7 +16,7 @@ file_line { 'Add redirection, 301':
   require => Package['nginx'],
 }
 
-file_line {'add header'.
+file_line {'add header':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
