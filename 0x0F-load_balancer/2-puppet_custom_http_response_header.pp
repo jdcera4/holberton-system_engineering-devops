51b492810c65709a48e1c9f3f92aee2c24e74446
jdcera4 <jdcera4@gmail.com>
2021-08-23 15:42:11 -0400

    add require

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,28 +1,32 @@
 # install ngnx whit puppet and configuration
 exec {'apt-get-update':
-  command => '/usr/bin/apt-get update'
+  command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
   ensure => installed,
+  require => Exec['apt-get update'],
 }
 
 file_line { 'Add redirection, 301':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
 }
 
 file_line {'add header'.
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $HOSTNAME;',
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $HOSTNAME;',
+  require => Package['nginx'],
 }
 
 file { '/var/www/html/index.html':
   content => 'Holberton School',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
