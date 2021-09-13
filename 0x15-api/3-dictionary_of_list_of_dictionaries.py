a2d530941aa1cde091313b0f6b9bb8749ade2208
jdcera4 <jdcera4@gmail.com>
2021-09-13 17:36:41 -0400

    correct code

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
+""" Module export to JSON all data
+"""
 import json
 import requests
-from sys import argv
-"""
-accessing a url with employee ID to return information
-"""
 
 
-if __name__ == "__main__":
-    users = requests.get("https://jsonplaceholder.typicode.com/users",
-                         verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos",
-                        verify=False).json()
-    user_dict = {}
-    usernamedict = {}
+if __name__ == '__main__':
+    # Url of api
+    api = 'https://jsonplaceholder.typicode.com/users/'
+    users = requests.get(api).json()
+    data = {}
     for user in users:
-        ID = user.get("id")
-        user_dict[ID] = []
-        usernamedict[ID] = user.get('username')
-    for task in todo:
-        task_dict = {}
-        ID = task.get("userId")
-        task_dict["task"] = task.get('title')
-        task_dict["completed"] = task.get('completed')
-        task_dict['username'] = usernamedict.get(ID)
-        user_dict.get(ID).append(task_dict)
-    with open("todo_all_employees.json", 'w') as jsfile:
-        json.dump(user_dict, jsfile)
+        # request for TODOs for each user
+        todo = requests.get(api + '{}/todos'.format(user['id'])).json()
+        # List of list all data I need export
+        data[user['id']] = [{
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': user['username']
+            } for task in todo]
+
+    # Write export file.json
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(data, f)
