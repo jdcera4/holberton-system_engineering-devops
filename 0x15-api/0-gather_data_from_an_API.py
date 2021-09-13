47859f5cc5e0d42eeedd3bd0463b810a9ce20af6
jdcera4 <jdcera4@gmail.com>
2021-09-13 15:50:25 -0400

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+"""Connect to api """
+
+
+if __name__ == "__main__":
+    id_empl = int(argv[1])
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(id_empl)).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(id_empl)).json()
+    tasks = []
+    for task in todos:
+        if task.get('completed') is True:
+            tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(tasks), len(todos)))
+    for task in tasks:
+        print("\t {}".format(task))
