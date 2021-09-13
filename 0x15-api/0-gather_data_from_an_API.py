40160a8c3e014114878bfa96a859d7b7c756464b
jdcera4 <jdcera4@gmail.com>
2021-09-13 15:57:24 -0400

    pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     id_empl = int(argv[1])
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
                         format(id_empl)).json()
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(id_empl)).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(id_empl)).json()
     tasks = []
     for task in todos:
         if task.get('completed') is True:
